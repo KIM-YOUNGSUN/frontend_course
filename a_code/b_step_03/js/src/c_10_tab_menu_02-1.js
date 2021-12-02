cdcdeec3df8bc3f4f5b9a99e56d6253317bad9a2
KIM-YOUNGSUN <sgi01272@gmail.com>
2021-12-02 23:21:01 +0900

    commit: updated a_code files and 211202_TIL

---
+++ a_code/b_step_03/js/src/c_10_tab_menu_02-1.js
@@ -0,0 +1,3 @@
+// c_10_tab_menu_02-1.js
+
+var innerCode = '<a href="#" data-id><h4 class="event_title"></h4><p class="event_narration"></p><dl class="date"><dt class="blind">기간</dt><dd></dd></dl><dl class="event_check success"><dt></dt><dd></dd></dl></a>';
