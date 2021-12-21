255d32443f0338e0e8403a32e8f764e573b975ac
KIM-YOUNGSUN <sgi01272@gmail.com>
2021-12-22 01:24:30 +0900

    commit: updated e_project_01 folder

---
+++ a_code/e_project_01/js/src/e_project.js
@@ -0,0 +1,14 @@
+// e_project.js
+
+const servicePart = document.querySelector('.sevice_part');
+const serviceBtn = document.querySelector('.sevice_part button');
+
+const onlinePart = document.querySelector('.online_part');
+const onlineBtn = document.querySelector('.online_part button');
+
+const companyinfoPart = document.querySelector('.company_info_part');
+const companyinfoBtn = document.querySelector('.company_info_part button');
+
+const legalPart = document.querySelector('.legal_part');
+const legalBtn = document.querySelector('.legal_part button');
+
