fd6418f5f2aed2e8fcf25343826772131d9e3ab6
KIM-YOUNGSUN <sgi01272@gmail.com>
2021-12-27 22:17:56 +0900

    commit: updated b_step_03 folder

--- a_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
+++ a_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
@@ -2,63 +2,113 @@
 
 // matchMedia를 이용하여 해당규격에 일치하는지(matches) 판단
 // window.matchMedia('css크기를 그대로 옮겨놓은 형태').matches 
+const wrap = document.querySelector('#wrap');
+const mobile = wrap.querySelector('.mobile');
+const pc = wrap.querySelector('.pc');
+mobile.remove();
+pc.remove();
 
-// const wrap = document.querySelector('#wrap');
-// const mobile = document.querySelector('.mobile');
-// const pc = document.querySelector('.pc');
-// mobile.remove();
-// pc.remove();
-// const size = 'screen and (max-width:768px)';
-// // const size2 = 'screen and (min-width:320px) and (max-width:768px)';
-// const mediaQuery = window.matchMedia(size);  
-// // 지정규격과 체크해서 맞으면 true 아니면 false
-// const fnCKMedia = ()=>{
-//   if(mediaQuery.matches){
-//     console.log('768보다 작은 크기');
-//     wrap.append(mobile);
-//     pc.remove();
-//   }else{
-//     console.log('768보다 큰 크기');
-//     wrap.append(pc);
-//     mobile.remove();
-//   }
-// };
-// fnCKMedia();
-// // ---------------------
-// // 정해진크기규격(max-width:768px)에서 변화할때 
-// mediaQuery.addEventListener('change', (e)=>{
-//   fnCKMedia();
-// });
+/*
+const size = 'screen and (max-width:768px)';
+const mediaQuery = window.matchMedia(size);  
+// 지정규격과 체크해서 맞으면 true 아니면 false
+const fnCKMedia = ()=>{
+  if(mediaQuery.matches){
+    console.log('768보다 작은 크기');
+    wrap.append(mobile);
+    pc.remove();
+  }else{
+    console.log('768보다 큰 크기');
+    wrap.append(pc);
+    mobile.remove();
+  }
+};
+fnCKMedia();
+// ---------------------
+// 정해진크기규격(max-width:768px)에서 변화할때 
+mediaQuery.addEventListener('change', (e)=>{
+  fnCKMedia();
+});
+*/
 
 // 이전버전에서는 무조건 브라우저의 크기가 변화할때마다(resize) 매번 확인해서 그 규격에 맞게 설정
 // matchMedia는 최초의 설정된 크기를 기준으로 해당내용이 변경(change)되면 바로 확인하여 설정
 
 
 // Question 어떻게 아래 규격을 확인할 것인가?
-const dataDevice = [
+const dataDevice = [ // dataDevice[n].size
   { type: 'mobile' , size : 599 }, // 600미만
   { type: 'tablet' , size : 767 }, // 600 ~ 767
   { type: 'laptop' , size : 1439 },// 768 ~ 1439
-  { type: 'pc' }                   // 1440 ~     
+  { type: 'pc'     }  // 1440 ~     
 ];
 
-const mobile = 'screen and (max-width:599px)';
-const tablet = 'screen and (min-width:600px) and (max-width:767px)';
-const laptop = 'screen and (min-width:768px) and (max-width:1439)';
-const pc = 'screen and (max-width:1440px)';
+// 규격 하나씩 작성 step1
+// const mobile = window.matchMedia(`screen and (max-width:${dataDevice[0].size}px)`);
+// const tablet = window.matchMedia(`screen and (min-width:${dataDevice[0].size + 1}px) and (max-width:${dataDevice[1].size}px)`);
+// const laptop = window.matchMedia(`screen and (min-width:${dataDevice[1].size + 1}px) and (max-width:${dataDevice[2].size}px)`);
+// const pc     = window.matchMedia(`screen and (min-width:${dataDevice[2].size + 1}px)`);
+// const mediaArray = [mobile, tablet, laptop, pc];
 
-mobile.addEventListener('change', e=>{
-  console.log('현재 mobile 규격에서 무언가 변화했습니다.');
+// 규격을 조금더 확장가능하도록 처리
+const mediaArray = [];
+for(let i=0; i<dataDevice.length; i+=1){
+  let matchCode;
+  if(i === 0){
+    matchCode = window.matchMedia(`screen and (max-width:${dataDevice[i].size}px)`);
+  }else if( i === dataDevice.length - 1){
+    matchCode = window.matchMedia(`screen and (min-width:${dataDevice[i-1].size + 1}px)`);
+  }else{
+    matchCode = window.matchMedia(`screen and (min-width:${dataDevice[i-1].size + 1}px) and (max-width:${dataDevice[i].size}px)`);
+  }
+  mediaArray.push(matchCode);
+}
+
+console.log( mediaArray );
+
+//=======================================================================================================================
+//이벤트 하나씩 작성 -step1
+// mobile.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 mobile규격'); }
+// });
+// tablet.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 tablet규격'); }
+// });
+// laptop.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 laptop규격'); }
+// });
+// pc.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 pc규격'); }
+// });
+
+const fnInsertCode = (type)=>{
+    switch(type){
+      case 'mobile':
+      case 'tablet':
+        wrap.append(mobile);
+        pc.remove();
+        break;
+      case 'laptop':
+      case 'pc':
+        wrap.append(pc);
+        mobile.remove();
+        break;
+    }
+}
+
+const fnCKDevice = (device, index)=>{
+  if(device.matches){ 
+    let type = dataDevice[index].type;
+    fnInsertCode(type);  
+  }
+}
+
+mediaArray.forEach( (type, index) =>  {     
+    fnCKDevice(type, index);
 });
 
-tablet.addEventListener('change', e=>{
-  console.log('현재 tablet 규격에서 무언가 변화했습니다.');
-});
-
-laptop.addEventListener('change', e=>{
-  console.log('현재 laptop 규격에서 무언가 변화했습니다.');
-});
-
-pc.addEventListener('change', e=>{
-  console.log('현재 pc 규격에서 무언가 변화했습니다.');
+mediaArray.forEach( (type,index) =>  {
+  type.addEventListener('change', (e) =>{
+    fnCKDevice(type, index);
+  });
 });
