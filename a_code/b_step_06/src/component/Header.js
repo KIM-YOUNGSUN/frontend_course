bb8677dca6731629e09fde3b3f1acf19dc4c09a8
KIM-YOUNGSUN <sgi01272@gmail.com>
2022-01-04 19:51:12 +0900

    commit: updated b_step_06 folder

--- a_code/b_step_06/src/component/Header.js
+++ a_code/b_step_06/src/component/Header.js
@@ -1,39 +1,12 @@
-// Header.js
+import Navigation from './Navigation';
+import '../style/Header.scss'
 
-export default function Header({site, url}){
-  // const props = {  site:'xido',  url : 'http://naver.com' }
-  // const myName = 'xido-school';
-  // const site = 'xido';
-  // const url = "http://naver.com";
-
-  const h1Style = { color:'#05f', textTransform: 'uppercase' };
-  const navLists = ['about','product','gallery','notice'];
-
+export default function Header(props){
+  
   return (
-      <header id="headBox">
-        <h1 style={h1Style}>
-          <a href={url} target="_blank">{site}</a>
-          </h1>
-          <nav className="gnb">
-            <h2>navigation</h2>
-            <ul>
-              {/*<li><a href="/about">about</a></li>
-              <li><a href="/product">product</a></li>
-              <li><a href="/gallery">gallery</a></li>
-              <li><a href="/notice">notice</a></li>*/}
-              {navLists.map((li, idx)=> <li key={idx}>{li}</li>)}
-            </ul>
-          </nav>
-      </header>
-  )
+    <header id="headBox">
+      <h1>{props.heading}</h1>
+      <Navigation />      
+    </header>
+    )
 }
-
-
-// -------------------------------------
-// function Header() {}
-// export default Header;
-
-// export default function Header(){}
-
-// const Header = function(){}
-// export default Header;
