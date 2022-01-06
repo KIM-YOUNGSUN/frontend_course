70590a052b180375ec99fe9699901bc011bbe395
KIM-YOUNGSUN <sgi01272@gmail.com>
2022-01-06 17:49:01 +0900

    commit: updated b_step_06 - react Link

--- a_code/b_step_06/src/component/Main.js
+++ a_code/b_step_06/src/component/Main.js
@@ -1,15 +1,12 @@
 import ConBox from './ConBox';
-import Review from './Review.jsx';
-import CardList from './CardList';
+
+
 
 export default function Main() {
   return (
     <main className="container">
         <section id="viewBox">section</section>
-        <ConBox />
-        <Review />
-        <CardList />
-        
+        <ConBox />              
     </main>
   )
 }
